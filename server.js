bba5af04cca1484dfc38ba6c34efd338195fef4a
bhvdgkireetivarma <79443593+bhvdgkireetivarma@users.noreply.github.com>
2021-09-27 19:07:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
+var obj={};
+
+io.on('connection',socket=>{
+    // when ever new user is joined
+    //client will send notification
+
+    socket.on('new-user-joined',name =>{
+        obj[socket.id]=name;
+socket.broadcast.emit('user-joined',name);
+
+    })
+    //when ever user sends message client will send notification
+    socket.on('send',message=>{
+        obj['message']=message;
+        socket.broadcast.emit('receive',{message:message,name:obj[socket.id]});
+        //will send it to all except the one who sends the request(client)
+    })
+    // whenever user disconnects it will be emitted automatically
+    socket.on('disconnect',message=>{
+      socket.broadcast.emit('left',obj[socket.id]);
+    })
+
+})
+
